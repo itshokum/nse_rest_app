95826de19592e1a99e5f73d13c9937fa8778982a
saurabh kumar <saurabhkumar@localhost.localdomain>
2018-05-27 23:08:10 +0530

    Trying with gunicorn

--- app.py
+++ app.py
@@ -6,21 +6,21 @@ import csv
 app = Flask(__name__)
 
 
-def create_stock_list_dict():
-	with open('EQUITY_L.csv','r',encoding='utf-8') as infile:
-		reader = csv.reader(infile)
-		stock_list_dict = {rows[0] : rows[1] for rows in reader}
-	return stock_list_dict
+#def create_stock_list_dict():
+#	with open('EQUITY_L.csv','r',encoding='utf-8') as infile:
+#		reader = csv.reader(infile)
+#		stock_list_dict = {rows[0] : rows[1] for rows in reader}
+#	return stock_list_dict
 
-stock_list_dict = create_stock_list_dict()
+#stock_list_dict = create_stock_list_dict()
 
 @app.route('/stock/<string:stock_symbol>')
 def get_stock_details(stock_symbol):	
 	#stock_symbol = 'INFY'
-	print(stock_symbol.upper())
-	if stock_symbol.upper() not in stock_list_dict:
-		message = "Stock with symbol {} not found".format(stock_symbol)
-		return jsonify({'message' : message})
+	#print(stock_symbol.upper())
+	#if stock_symbol.upper() not in stock_list_dict:
+	#	message = "Stock with symbol {} not found".format(stock_symbol)
+	#	return jsonify({'message' : message})
 
 	nse_quote_url = 'https://nseindia.com/live_market/dynaContent/live_watch/get_quote/GetQuote.jsp?symbol=' + urllib.parse.quote(stock_symbol)
 
